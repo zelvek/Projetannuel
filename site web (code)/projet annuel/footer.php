15b9940c8fd6ffcc8a51d941b9ed45d5d6b9e359
zelvek <zelvekalois@outlook.com>
2017-03-04 12:59:58 +0100

    ajout
    back

--- site web (code)/projet annuel/footer.php
+++ site web (code)/projet annuel/footer.php
@@ -1,7 +1,6 @@
-<footer>
-<div class="row">
-    <div class="col-lg-12 copyright">
-        <p>Copyright &copy; ZelveK, Evenn, Louis  </p>
-    </div>
-</div>
-</footer>
+    <footer>
+        @Lectus Project
+    </footer>
+
+  </body>
+</html>
