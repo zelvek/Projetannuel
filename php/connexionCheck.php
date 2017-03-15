872683a901ff7ed105d8248812f205f467df4917
ulphidius <louis_laurent@aol.fr>
2017-03-15 09:44:24 +0100

    1.1
    Galère

--- php/connexionCheck.php
+++ php/connexionCheck.php
@@ -7,16 +7,23 @@ if (count($_POST) == 2
   && !empty($_POST["checkPseudo"])
   && !empty($_POST["checkPass"])
 ){
-  $activation = new PDO('mysql:host = '.DB_HOST.'; dbname =  '.DB_NAME, DB_USER, DB_PWD);
+  try{
+    $activation = new PDO('mysql:host = '.DB_HOST.'; dbname =  '.DB_NAME, DB_USER, DB_PWD);
+  }
+  catch(Exception $verif){
+      die("Probleme connexion serveur :" .$verif->getMessage());
+  }
   $_POST["name"] = trim($_POST["checkPseudo"]);
-  $recherchePseudo = $activation query->('SELECT nickname FROM esgi1i2 WHERE nickname = '$_POST["checkPseudo"]);
+  $recherchePseudo = $activation->prepare('SELECT nickname FROM esgi1i2 WHERE nickname = ?');
+  $recherchePseudo->execute($_POST["checkPseudo"]);
   if ($recherchePseudo == false){
     die("Erreur pseudo inexistant");
 
 
   }
   else if($recherchePseudo != false ){
-      $recherchePass = $activation query->('SELECT pwd FROM esgi1i2 WHERE nickname = '$_POST["checkPseudo"] );
+      $recherchePass = $activation->prepare('SELECT pwd FROM esgi1i2 WHERE nickname = ?' );
+      $recherchePass->execute($_POST["checkPseudo"]);
       $result = password_hash($_POST["checkPass"], $recherchePass);
     }
 
