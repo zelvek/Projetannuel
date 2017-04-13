1c7b378cbf1061b20f6d7e029c55a492c2232924
zelvek <zelvekalois@outlook.com>
2017-04-13 02:54:01 +0200

    10.222
    5656

--- option.php
+++ option.php
@@ -11,18 +11,11 @@
 
     <title>LECTUS</title>
 
-    <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom CSS -->
+
     <link href="css/one-page-wonder.css" rel="stylesheet">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
 
 </head>
 
@@ -34,25 +27,24 @@
 
 
 
-    <!-- Page Content -->
+
     <div class="container">
 
   <?php include("part/utilisateur.php") ?>
 
         <hr class="featurette-divider">
 
-        <!-- Footer -->
+
 
             <?php include("part/footer.php"); ?>
 
 
     </div>
-    <!-- /.container -->
 
-    <!-- jQuery -->
+
     <script src="js/jquery.js"></script>
 
-    <!-- Bootstrap Core JavaScript -->
+
     <script src="js/bootstrap.min.js"></script>
 
 </body>
