b9e9a328c2634d77ecaac9e442ba891cd9f6c37a
zelvek <zelvekalois@outlook.com>
2017-04-21 15:02:52 +0200

    qss
    ss

--- index.php
+++ index.php
@@ -43,16 +43,21 @@
     </script>
 
 
-  <!-- google pub -->
-
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <script>
-      (adsbygoogle = window.adsbygoogle || []).push({
-        google_ad_client: "ca-pub-3323508660354393",
-        enable_page_level_ads: true
-      });
-    </script>
-
+    <!-- Piwik -->
+  <script type="text/javascript">
+    var _paq = _paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u="//localhost/Projetannuel/admin/";
+      _paq.push(['setTrackerUrl', u+'piwik.php']);
+      _paq.push(['setSiteId', '1']);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+  </script>
+  <!-- End Piwik Code -->
 
 
 
