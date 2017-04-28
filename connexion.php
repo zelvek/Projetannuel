34b0189d99f015d903c252d780958ee2ae0106a9
zelvek <zelvekalois@outlook.com>
2017-04-28 12:51:55 +0200

    azert
    azer

--- connexion.php
+++ connexion.php
@@ -7,7 +7,7 @@ include "part/header.php";
    <h1>Connexion</h1>
    <form method = "post" action = "php/connexionCheck.php">
      <input type = "text" name = "email" placeholder = "Votre email" value = "" required = "required" ><br>
-     <input type = "text" name = "pwd" placeholder = "Votre Password" value = "" require = "required" ><br>
+     <input type = "password" name = "pwd" placeholder = "Votre Password" value = "" require = "required" ><br>
      <input type ="submit" value="Connexion">
    </form>
  </section>
