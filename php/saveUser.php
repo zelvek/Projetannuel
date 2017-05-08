6faeed3251aa2a0ca4f8f6dcb21534cca75d10f8
zelvek <zelvekalois@outlook.com>
2017-05-08 16:51:32 +0200

    qzd
    qzd

--- php/saveUser.php
+++ php/saveUser.php
@@ -1,8 +1,24 @@
+Skip to content
+This repository
+Search
+Pull requests
+Issues
+Gist
+ @zelvek
+ Sign out
+ Unwatch 1
+  Unstar 1
+  Fork 0 zelvek/Projetannuel
+ Code  Issues 1  Pull requests 1  Projects 1  Wiki  Pulse  Graphs  Settings
+Tree: 990cf8489f Find file Copy pathProjetannuel/php/saveUser.php
+990cf84  22 minutes ago
+@zelvek zelvek save user final
+1 contributor
+RawBlameHistory
+183 lines (132 sloc)  3.73 KB
 <?php
 require "config.php";
 session_start();
-
-
 if( count($_POST)==11
 && !empty($_POST["gender"])
 && isset($_POST["name"])
@@ -15,26 +31,18 @@ if( count($_POST)==11
 && !empty($_POST["country"])
 && !empty($_POST["legacy"])
 && !empty($_POST["captcha"])){
-
   $error = false;
   $listOfErrors = [];
   try{
-
   $db = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER,DB_PWD);
-
   }catch(Exception $e){
   die("erreur SQL :".$e->getMessage());
-
   }
-
 $_POST["name"] = trim($_POST["name"]);
 $_POST["nickname"] = trim($_POST["nickname"]);
 $_POST["surname"] = trim($_POST["surname"]);
 $_POST["birthday"] = trim($_POST["birthday"]);
 $_POST["email"] = trim($_POST["email"]);
-
-
-
 if (strlen($_POST["name"])<2 || strlen($_POST["name"])> 50  ){
   $error = true;
   $listOfErrors[] = 1;
@@ -47,110 +55,80 @@ if (strlen($_POST["nickname"])<2 || strlen($_POST["nickname"])> 50) {
   $error = true;
   $listOfErrors[] = 3;
 }
-
 if (strlen($_POST["pwd"])<8 || strlen($_POST["pwd"])> 500) {
 $error = true;
 $listOfErrors[] = 4;
 }
-
 if ($_POST["pwd"] != $_POST["pwd2"]) {
   $error = true;
   $listOfErrors[] = 5;
 }
-
 if(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
   $error = true;
   $listOfErrors[] = 6;
 }
-
 if ($_SESSION["captcha"] != $_POST["captcha"] ) {
   $error = true;
   $listOfErrors[] = 7;
 }
-
 if (!array_key_exists($_POST["gender"],$listOfGender)) {
 $error = true;
 $listOfErrors[] = 8;
-
 }
 if (!array_key_exists($_POST["country"],$listOfCountry)) {
 $error = true;
 $listOfErrors[] = 9;
-
 }
-
 if( strlen($_POST["birthday"]) == 10 ){
-
 		if( substr_count($_POST["birthday"], "/") == 2){
 			$arrayBirthday = explode("/", $_POST["birthday"]);
-
 			$year = $arrayBirthday[2];
 			$month = $arrayBirthday[1];
 			$day = $arrayBirthday[0];
-
-
 		}
       else if( substr_count($_POST["birthday"], "-") == 2){
-
 			$arrayBirthday = explode("-", $_POST["birthday"]);
-
 			$year = $arrayBirthday[0];
 			$month = $arrayBirthday[1];
 			$day = $arrayBirthday[2];
-
-
 		}
     else{
 			$error = true;
 			$listOfErrors[] = 10;
 		}
-
 	 }
    else{
 		$error = true;
 		$listOfErrors[] = 11;
 	 }
-
-
-
 	if( !empty($year) && !empty($month) && !empty($day) && checkdate($month,$day,$year )
  ){
 		$oneYear = 365*24*60*60;
 		$time100Year = time() - $oneYear*100;
 		$time10Year = time() - $oneYear*10;
 		$timeBirthday = strtotime($year."-".$month."-".$day);
-
 		if($timeBirthday<$time100Year || $timeBirthday>$time10Year ) {
 			$error = true;
 			$listOfErrors[] = 12;
 		}
-
 	}else{
 		$error = true;
 		$listOfErrors[] = 13;
 	}
-
-
-
   $query = $db->prepare('SELECT Id FROM users WHERE Email=:Email');
   $query->execute(["Email" => $_POST["email"]]);
   $resultat = $query->fetch();
-
   if( !empty($resultat)){
     $error = true;
     $listOfErrors[] = 14;
   }
-
 if($error) {
   $_SESSION["errors_form"] = $listOfErrors;
   $_SESSION["data_form"] = $_POST;
 header('Location: ../index.php');
-
 }else {
-
 $query = $db->prepare("INSERT INTO users (Gender,Name,Surname,Nickname,Pwd,Email,Birthday,Country) VALUES (:Gender ,:Name ,:Surname ,:Nickname ,:Pwd ,:Email ,:Birthday ,:Country )");
 $pwd = password_hash($_POST["pwd"], PASSWORD_DEFAULT);
-
 $query->execute([
   "Gender"=>$_POST["gender"],
 "Name"=>$_POST["name"],
@@ -161,8 +139,12 @@ $query->execute([
 "Birthday"=>$year."-".$month."-".$day,
 "Country"=>$_POST["country"] ]);
 //header( 'Location ../index2.php');
-
-
+$query = $db->prepare("INSERT INTO tchat (pseudo, message) VALUES (:pseudo, :message )");
+$e = 'inscription';
+$query->execute([
+  "pseudo"=>$_POST["email"],
+"message"=>$e ]);
+print_r($query->errorInfo());
 header("Location: ../index.php");
-//aaaaa
   }
+}
