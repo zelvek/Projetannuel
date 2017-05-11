471958cb890977657e25c830ca91cb9fe2e74133
zelvek <zelvekalois@outlook.com>
2017-05-11 18:24:20 +0200

    qzd
    qzd

--- test.php
+++ test.php
@@ -13,12 +13,13 @@ $url2 = "https://api.whatismymovie.com/1.0/?api_key=TmhiLZMKtVSbsgfU&refinements
 // Set some options - we are passing in a useragent too here
 curl_setopt_array($curl, array(
     CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'https://api.whatismymovie.com/1.0/?api_key=TmhiLZMKtVSbsgfU&refinements=enabled&test=men+in+black',
+    CURLOPT_URL => 'https://api.whatismymovie.com/1.0/?api_key=TmhiLZMKtVSbsgfU&text=drummer+of+the+band+explodes,+there+is+stonehenge+and+amplifier+going+to+eleven',
     CURLOPT_USERAGENT => 'Codular Sample cURL Request',
-     CURLOPT_HTTPGET => true,
+     CURLOPT_HTTPGET => true
 ));
 // Send the request & save response to $resp
 $resp = curl_exec($curl);
+//https://api.whatismymovie.com/1.0/?api_key=TmhiLZMKtVSbsgfU&text=drummer+of+the+band+explodes,+there+is+stonehenge+and+amplifier+going+to+eleven
 
 
 if(!curl_exec($curl)){
@@ -32,7 +33,7 @@ print_r($resp);
 
 var_dump( $resp );
 
-
+curl_getinfo($curl);
 echo "<br>";
 json_decode($resp, true);
 
