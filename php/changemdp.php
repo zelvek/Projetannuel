90734c24cbfd0038b18cda23623cd077dfd91059
zelvek <zelvekalois@outlook.com>
2017-05-11 20:57:03 +0200

    aze
    aze

--- php/changemdp.php
+++ php/changemdp.php
@@ -1,8 +1,33 @@
 <?php
 
+
+
+
 require "../php/config.php";
 
 session_start();
+
+
+
+
+                          try{
+                              $db = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME , DB_USER, DB_PWD); // /!\ connection à la base de données /!\
+                          }catch(Exception $e){
+                            die("Erreur SQL : ".$e->getMessage() );
+                          }
+
+                          $query = $db->prepare('SELECT Is_admin FROM users WHERE email = :email;');
+                            $query->execute(["email" =>$_SESSION['email']] );
+
+
+                            $w = $query->fetch()["Is_admin"];
+
+
+                            if ($w == 1) {
+
+
+
+
 
 if (count($_POST) ==2
 && !empty($_POST["mdp"])
@@ -67,7 +92,9 @@ echo "test";
 
 
 
-
+}else {
+  header('Location: ../index.php');
+}
 
 
 
